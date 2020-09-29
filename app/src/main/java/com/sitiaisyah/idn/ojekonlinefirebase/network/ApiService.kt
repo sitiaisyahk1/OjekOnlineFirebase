7a9b2e46d24de5bfd8484b989eb152ee6cf46f35
Sitiaisyah K <sitiaisyahkg@gmail.com>
2020-09-29 11:35:09 +0700

    finish

--- app/src/main/java/com/sitiaisyah/idn/ojekonlinefirebase/network/ApiService.kt
+++ app/src/main/java/com/sitiaisyah/idn/ojekonlinefirebase/network/ApiService.kt
@@ -13,7 +13,7 @@ interface ApiService {
                     @Query("key")key: String):Flowable<ResultRoute>
 
     @Headers(
-        "Authorization: key=AAAAkKralYs:APA91bENYCWMi7iSpj-3eX7AFl98mn7kJcv9Z2jVFJPRtdY7vyxpft7V6putK05aLK13FmTpAyGG_GQNZlCfXfsbwAa2cz9jNCocQtlZl0Ox4J9pXYQZZ6h6W6kqYJSDQRjsKhcsM_K7",
+        "Authorization: key=AAAA2K2-PlA:APA91bFQqk1jg-6tmLEkMbAK_8aVBSDn1adWRH2vJRgHZ5st1HxjI4SGvELC0jiPflzNiKH2htv8fwCRd__flib03IyyNUh2qT7QtVh5Prw87EZkKspxpQzWAana1iAKkqU1BSdZhEAT",
         "Content-Type:application/json"
     )
     @POST("fcm/send")
